57ee98d0f6437f6357c239daf1716505c2cfdc1c
etec <etec@L05M10>
2025-04-28 20:30:05 -0300

    Adicionar arquivos de projeto.

---
+++ Automacao01_PodeVotar/Voto.cs
@@ -0,0 +1,12 @@
+﻿namespace Automacao01_PodeVotar;
+
+public class Voto
+{
+    public string podeVotar(int idade)
+    {
+        if (idade < 16) return "Não pode votar!";
+        if (idade >= 18) return "Pode votar!";
+        return "Voto opcional!";
+    }
+}
+
---
+++ Automacao01_PodeVotar_Test/VotoUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using Automacao01_PodeVotar;
+
+namespace Automacao01_PodeVotar_Test
+{
+    [TestClass]
+    public sealed class VotoUnitTest
+    {
+        [TestMethod]
+        public void Teste_Idade21_DevePoderVotar()
+        {
+            //Arrange - Preparação do ambiente de teste
+            Voto voto = new Voto();
+            int idade = 21;
+            string esperado = "Pode votar!";
+
+            //Act - Execução da chamada do artefato a ser testado
+            string obtido = voto.podeVotar(idade);
+
+            //Assert - Comparando resultados para saber se passou
+            Assert.AreEqual(esperado, obtido);
+        }
+
+        [TestMethod]
+        public void Teste_Idade18_DevePoderVotar()
+        {
+            //Arrange - Preparação do ambiente de teste
+            Voto voto = new Voto();
+            int idade = 18;
+            string esperado = "Pode votar!";
+
+            //Act - Execução da chamada do artefato a ser testado
+            string obtido = voto.podeVotar(idade);
+
+            //Assert - Comparando resultados para saber se passou
+            Assert.AreEqual(esperado, obtido);
+        }
+    }
+}
