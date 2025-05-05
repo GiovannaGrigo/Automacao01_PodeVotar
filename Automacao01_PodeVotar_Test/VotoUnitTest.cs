be2ae8c4b1d280f49e435746a0c3cae413499ef7
giovannagrigo <giovanna1grigo@gmail.com>
2025-05-05 20:24:44 -0300

    Dinamização dos testes unitários

--- Automacao01_PodeVotar_Test/VotoUnitTest.cs
+++ Automacao01_PodeVotar_Test/VotoUnitTest.cs
@@ -5,11 +5,19 @@ namespace Automacao01_PodeVotar_Test
     [TestClass]
     public sealed class VotoUnitTest
     {
+        private Voto voto;
+
+        [TestInitialize]
+        public void setup()
+        {
+            voto = new Voto();
+        }
+
         [TestMethod]
         public void Teste_Idade21_DevePoderVotar()
         {
             //Arrange - Preparação do ambiente de teste
-            Voto voto = new Voto();
+            //Voto voto = new Voto();
             int idade = 21;
             string esperado = "Pode votar!";
 
@@ -24,9 +32,27 @@ namespace Automacao01_PodeVotar_Test
         public void Teste_Idade18_DevePoderVotar()
         {
             //Arrange - Preparação do ambiente de teste
-            Voto voto = new Voto();
+            //Voto voto = new Voto();
             int idade = 18;
             string esperado = "Pode votar!";
+
+            //Act - Execução da chamada do artefato a ser testado
+            string obtido = voto.podeVotar(idade);
+
+            //Assert - Comparando resultados para saber se passou
+            Assert.AreEqual(esperado, obtido);
+        }
+
+        [TestMethod]
+        [DataRow(15, "Não pode votar!")]
+        [DataRow(16, "Voto opcional!")]
+        [DataRow(17, "Voto opcional!")]
+        [DataRow(18, "Pode votar!")]
+        [DataRow(21, "Pode votar!")]
+        public void Teste_Idade_DeveRetornarStatusVoto(int idade, string esperado)
+        {
+            //Arrange - Preparação do ambiente de teste
+            //Voto voto = new Voto();
 
             //Act - Execução da chamada do artefato a ser testado
             string obtido = voto.podeVotar(idade);
